97761d46aa4b33d5af288b23c01c60305daee38a
Venturing Guy <LVentura335@Gmail.com>
2021-09-06 17:05:10 -0700

    API integration

--- src/Weather.js
+++ src/Weather.js
@@ -7,6 +7,8 @@ function Weather() {
     const UNIT_IDS = ['Metric', 'Imperial', 'Standard']
     const UNIT_LABELS = ['Celsius', 'Fahrenheit', 'Kelvin']
     
+    // Functions relating to the unitRadio components
+    // ----------------------------------------------
     const UnitRadio = (props) => {
         const {name, label} = props
         return (
@@ -31,7 +33,10 @@ function Weather() {
         const name="unit"
         const label = `${UNIT_IDS[index]}`
         return renderUnitRadio(name, label)
-      });
+    });
+
+    // Functions relating to the unitSelect option components
+    // ------------------------------------------------------
 
     const UnitSelect = (props) => {
         const {name, label} = props
@@ -50,15 +55,29 @@ function Weather() {
         const name= `${UNIT_IDS[index]}`
         const label = `${UNIT_LABELS[index]}`
         return renderUnitSelect(name, label)
-      });
+    });
+
+    // Fetches the weather upon submitting the form
+    // --------------------------------------------
+    const fetchWeather = async () => {
+        // fetch weather
+        const apiKey = '8dc260bebb36d0633ea80322c7425314'
+        const path = `https://api.openweathermap.org/data/2.5/weather?zip=${zip}&appid=${apiKey}&units=${unit}`
+        const res = await fetch(path)
+        const json = await res.json()
+
+        console.log(json)
+        // setData(newData)
+        const temp = json.main.temp
+        return temp
+    }
 
     return (
         <div className="Weather">
-            <h1>{zip} {unit}</h1>
-            <form obSubmit={e => {
+            {data && <h1>{data.main.temp}</h1>}
+            <form onSubmit={e => {
                 e.preventDefault()
-                // load weather data
-                // setData(newData)
+                fetchWeather()
             }}>
                 <div>
                     <input 
